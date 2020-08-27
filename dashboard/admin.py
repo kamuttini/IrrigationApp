7b1659386423a5991b6604c82ca09adc0d9366b9
Camilla <camilla.cantiani@gmail.com>
2020-08-27 18:36:45 +0200

    Location in admin panel

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Garden, Area
+from .models import Garden, Area, Location
 
 admin.site.register(Garden)
 admin.site.register(Area)
+admin.site.register(Location)
 
