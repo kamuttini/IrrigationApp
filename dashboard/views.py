66c93fa85ad5d74c16d6370784080756acb5d10d
Camilla <camilla.cantiani@gmail.com>
2020-08-27 18:27:53 +0200

    homepage displays gardens list

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Area
+from .models import Garden
 
 # Create your views here.
 def index(request):
-    latest_irrigation_list = Area.objects.order_by('last_irrigation')[:5]
-    context = {'latest_irrigation_list': latest_irrigation_list}
+    garden_list = Garden.objects.order_by('name')[:5]
+    context = {'garden_list': garden_list}
     return render(request, 'dashboard/index.html', context)
 
 def detail(request, area_id):
