ffc1eda25f69a8ce2aa3161c15d0c9c78c2ca45c
Riccardo <ricardo97@live.it>
2020-08-31 11:50:53 +0200

    Added get_weather_info to methods

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .forms import GardenForm, AreaForm, EventForm
-from .models import Area, Garden, Location
-from .static.translation import WEATHER
-import requests
+from .models import Area, Garden
 
 
 # Create your views here.
@@ -91,7 +89,7 @@ def garden_delete(request, garden_id):
         'object': obj,
         'garden_list': garden_list,
     }
-    return render(request, "dashboard/garden_delete.html", context)
+    return render(request, "dashboard/delete.html", context)
 
 
 def area_delete(request, area_id):
@@ -106,40 +104,11 @@ def area_delete(request, area_id):
         'object': obj,
         'garden_list': garden_list,
     }
-    return render(request, "dashboard/area_delete.html", context)
-
-
-def get_weather_info(location):
-    url = "https://api.climacell.co/v3/weather/forecast/daily"
-    querystring = {"lat": location.lat, "lon": location.lon, "unit_system": "si", "start_time": "now",
-                   "fields": "precipitation_probability,temp,weather_code",
-                   "apikey": "6KIHRzYEnmDjo2nD68e6GWlHYGfbRIO2"}
-
-    response = requests.request("GET", url, params=querystring).json()
-
-    weather_info = {
-        'location': location,
-        'temperature_min': str(response[0]['temp'][0]['min']['value']) + response[0]['temp'][0]['min']['units'],
-        'temperature_max': str(response[0]['temp'][1]['max']['value']) + response[0]['temp'][1]['max']['units'],
-        'precipitation': str(response[0]['precipitation_probability']['value']) +
-                         response[0]['precipitation_probability']['units'],
-        'description': response[0]['weather_code']['value']
-    }
-
-    image_icon = weather_info['description'] + ".svg"
-    weather_icons_path = "images/weather icons/color/" + image_icon
-
-    # translation
-    weather_info['description'] = (WEATHER[weather_info['description']])
-
-    context = {
-        'weather_info': weather_info,
-        'icon_path': weather_icons_path
-    }
-    return context
+    return render(request, "dashboard/delete.html", context)
 
 
 def weather(request):
+    from .methods import get_weather_info
     garden_list = Garden.objects.order_by('name')
     weather_list = []
     locations = []
