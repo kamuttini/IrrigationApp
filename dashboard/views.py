d0425b90e619d37291f2ecc54a7f03fab5fe63e2
Camilla <camilla.cantiani@gmail.com>
2020-08-31 10:47:04 +0200

    weather view changed

--- dashboard/views.py
+++ dashboard/views.py
@@ -109,16 +109,8 @@ def area_delete(request, area_id):
     return render(request, "dashboard/area_delete.html", context)
 
 
-def weather(request):
-    garden_list = Garden.objects.order_by('name')
-    garden = get_object_or_404(Garden)
-
-
-    location_list = Location.objects.order_by('city')
-    location = get_object_or_404(Location)
-
+def get_weather_info(location):
     url = "https://api.climacell.co/v3/weather/forecast/daily"
-
     querystring = {"lat": location.lat, "lon": location.lon, "unit_system": "si", "start_time": "now",
                    "fields": "precipitation_probability,temp,weather_code",
                    "apikey": "6KIHRzYEnmDjo2nD68e6GWlHYGfbRIO2"}
@@ -126,6 +118,7 @@ def weather(request):
     response = requests.request("GET", url, params=querystring).json()
 
     weather_info = {
+        'location': location,
         'temperature_min': str(response[0]['temp'][0]['min']['value']) + response[0]['temp'][0]['min']['units'],
         'temperature_max': str(response[0]['temp'][1]['max']['value']) + response[0]['temp'][1]['max']['units'],
         'precipitation': str(response[0]['precipitation_probability']['value']) +
@@ -133,22 +126,33 @@ def weather(request):
         'description': response[0]['weather_code']['value']
     }
 
-    weather_icons_path = "images/weather icons/color/"
     image_icon = weather_info['description'] + ".svg"
-    icon_path = weather_icons_path + image_icon
+    weather_icons_path = "images/weather icons/color/" + image_icon
 
     # translation
     weather_info['description'] = (WEATHER[weather_info['description']])
 
     context = {
-        'location_list': location_list,
-        'location': location,
-        'garden': garden,
-        'garden_list': garden_list,
         'weather_info': weather_info,
-        'icon_path': icon_path
+        'icon_path': weather_icons_path
     }
+    return context
 
+
+def weather(request):
+    garden_list = Garden.objects.order_by('name')
+    weather_list = []
+    locations = []
+
+    for garden in garden_list:
+        if garden.city not in locations:
+            weather_list.append(get_weather_info(garden.city))
+            locations.append(garden.city)
+
+    context = {
+        'garden_list': garden_list,
+        'weather_info': weather_list,
+    }
     return render(request, "dashboard/weather.html", context)
 
 
@@ -167,4 +171,3 @@ def garden_update(request, garden_id):
     }
 
     return render(request, "dashboard/update.html", context)
-
