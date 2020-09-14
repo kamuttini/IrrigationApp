03c38a5f5c57ebe25ba5b02b87e7ced56d7fc0b6
Camilla <camilla.cantiani@gmail.com>
2020-09-15 00:09:26 +0200

    notification for low humidity

--- notification/models.py
+++ notification/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.utils import timezone
+from dashboard.models import Area
+
 
 # Create your models here.
+
 class Notification(models.Model):
     title = models.CharField(max_length=256)
     message = models.TextField()
@@ -18,3 +21,10 @@ def create_welcome_message(sender, **kwargs):
         Notification.objects.create(user=kwargs.get('instance'),
                                     title="Benvenuto nella Dashboard",
                                     message="Grazie per esserti registrato")
+
+@receiver(pre_save, sender=Area)
+def notify_low_humidity(sender, instance, **kwargs):
+    if instance.humidity < 30:
+        Notification.objects.create(user=instance.garden.user,
+                                    title="Umidità bassa",
+                                    message="umidità sotto il 30%")
