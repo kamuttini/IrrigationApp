a8242fcb98600bfb2608f12916af2b9f00d782f8
Camilla <camilla.cantiani@gmail.com>
2020-09-17 13:45:06 +0200

    delete bottom removed in new event and agenda urls simplified

--- agenda/urls.py
+++ agenda/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'agenda'
 urlpatterns = [
     url(r'^$', views.CalendarView.as_view(), name='calendar'),
-    path('event/new/', views.event, name='event_new'),
-    path('event/edit/<event_id>/', views.event, name='event_edit'),
-    path('event/delete/<event_id>/', views.event_delete, name='event_delete')
+    path('new/', views.event, name='event_new'),
+    path('<event_id>/edit/', views.event, name='event_edit'),
+    path('<event_id>/delete/', views.event_delete, name='event_delete')
 ]
