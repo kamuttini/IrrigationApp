181b44d6ddef396cc90f06dbca996bb4d7eec70d
Camilla <camilla.cantiani@gmail.com>
2020-09-17 15:26:04 +0200

    update and create view reduced

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,11 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:garden_id>/', views.detail, name='detail'),
-    path('<int:garden_id>/garden_update/', views.garden_update, name='garden_update'),
-    path('<int:area_id>/area_update/', views.area_update, name='area_update'),
+    path('<int:id>/update/<slug:type>', views.update, name='update'),
     path('<int:area_id>/manual_irrigation/', views.manual_irrigation, name='manual_irrigation'),
-    path('garden_create/', views.garden_create, name='garden_create'),
-    path('<int:garden_id>/area_create/', views.area_create, name='area_create'),
+    path('garden_create/', views.create, name='garden_create'),
+    path('<int:garden_id>/area_create/', views.create, name='area_create'),
     path('<int:id>/delete/<slug:type>', views.delete, name='delete'),
     path('<int:area_id>/area_detail/', views.area_detail, name='area_detail'),
     path('weather/', views.weather, name='weather')
