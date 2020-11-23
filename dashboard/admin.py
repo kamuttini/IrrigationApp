a266417bbff484ee775dcbd071b39b4250c75c55
Camilla <camilla.cantiani@gmail.com>
2020-11-23 22:26:29 +0100

    minor improvements

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
 
@@ -10,3 +11,17 @@ admin.site.register(Location)
 admin.site.register(Irrigation)
 admin.site.register(ScheduledIrrigation)
 admin.site.register(Setting)
+
+
+class SettingInline(admin.StackedInline):
+    model = Setting
+    can_delete = False
+    verbose_name_plural = 'setting'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (SettingInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
